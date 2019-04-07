dbc54c8a2e10e84e52d375860bf679cf9fe36b21
Michał Darda <michaldarda@gmail.com>
2019-04-07 22:59:13 +0200

    Define new abbrevs

--- emacs/abbrevs.el
+++ emacs/abbrevs.el
@@ -2,3 +2,9 @@
   '(
     ("pry" "require 'pry'; binding.pry # FIXME")
     ))
+
+(define-abbrev-table 'js2-mode-abbrev-table
+  '(
+    ("clog" "console.log(\"Something\") // eslint-disable-line")
+    ("debug" "debugger // eslint-disable-line")
+    ))
