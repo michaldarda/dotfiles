32903567019d087ed006ca65d616fba33744333f
Michal Darda <mdarda@pm.me>
2019-09-23 01:38:56 +0200

    new aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -7,5 +7,7 @@ alias gch 'git checkout'
 alias ga 'git add'
 alias gca 'git commit -a -m'
 alias gc 'git commit -m'
+alias gd 'git diff'
+alias gds 'git diff --staged'
 
 alias rl '. ~/.config/fish/config.fish'
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
 . ~/.config/fish/aliases.fish
 
 set -gx fish_greeting ''
+status --is-interactive; and source (jump shell fish | psub)
