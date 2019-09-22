769c2552294df526e98debac0f3db03c36e8821d
Michal Darda <mdarda@pm.me>
2019-09-23 01:35:19 +0200

    some useful aliases

---
+++ fish/aliases.fish
@@ -0,0 +1,11 @@
+alias ll 'ls -ltra'
+
+alias vi vim
+
+alias gst 'git status'
+alias gch 'git checkout'
+alias ga 'git add'
+alias gca 'git commit -a -m'
+alias gc 'git commit -m'
+
+alias rl '. ~/.config/fish/config.fish'
